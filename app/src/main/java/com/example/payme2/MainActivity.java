aedf6cc6189fca30afa13b184aaa471a786cc167
shraddha510 <svp140330@utdallas.edu>
2023-12-03 02:32:05 -0600

    Add group list button

--- app/src/main/java/com/example/payme2/MainActivity.java
+++ app/src/main/java/com/example/payme2/MainActivity.java
@@ -83,5 +83,6 @@ public class MainActivity extends AppCompatActivity {
         editor.apply();
 
         Toast.makeText(MainActivity.this, "User Registered!", Toast.LENGTH_SHORT).show();
+        navigateToGroupsActivity();
     }
 }
